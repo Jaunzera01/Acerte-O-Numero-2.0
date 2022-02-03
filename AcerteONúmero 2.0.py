30a031a359a2cec2c6a889925abce5a5c5d574e7
João Pedro <89290727+Jaunzera01@users.noreply.github.com>
2022-02-03 17:02:54 -0300

    Update AcerteONúmero 2.0.py

--- AcerteONúmero 2.0.py
+++ AcerteONúmero 2.0.py
@@ -5,7 +5,7 @@ def gera():
 
 #Jogo:
 numeroaleatorio = gera()
-print('Tente adivinhar um número aleatório de 0 à 25')
+print('Tente adivinhar um número aleatório de 0 à 25, a maioria das pessoas acertam com 5 tentativas!')
 jogada = int(input('Qual a sua ideia?: '))
 cont = 1
 
